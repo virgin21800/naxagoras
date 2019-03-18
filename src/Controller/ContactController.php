49cb17b6741543e823b4ddae6232807982ea0119
asma <bouacha.asma@gmail.com>
2019-03-18 14:41:48 +0100

    rien

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ContactController extends AbstractController
+{
+    // /**
+    //  * @Route("/contact", name="contact")
+    //  */
+    // public function index()
+    // {
+    //     $form = $this->createForm(ContactType::class);
+
+    //     return $this->render('contact/form.html.twig', [ 'our_form' => $form,]);
+    // }
+    // /** 
+    // * * Creates and returns a Form instance from the type of the form. * * @final since version 3.4 
+    // * */
+    // protected function createForm(string $type, $data = null, array $options = array()): FormInterface
+    // {
+
+    // }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+//     public function buildForm(FormBuilderInterface $builder, array $options)
+//     {
+//         $builder
+//             ->add('name') 
+//             ->add('from', EmailType::class) 
+//             ->add('message', TextareaType::class)  ;
+//     }
+
+//     public function configureOptions(OptionsResolver $resolver)
+//     {
+//         $resolver->setDefaults([
+//             // Configure your form options here
+//             // 'data_class' => User::class,
+//         ]);
+//     }
+ }
